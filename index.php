d01973c7aeef5627a1663ccf394b2dc89dc2c045
Ego <augurto.17@gmail.com>
2023-08-10 16:08:08 -0500

    boostrap

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Precios con y sin IGV</title>
+</head>
+<body>
+  <div class="container mt-5">
+    <h2>Tabla de Precios con y sin IGV</h2>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Descripción</th>
+          <th>Precio sin IGV</th>
+          <th>Tasa de IGV</th>
+          <th>Monto de IGV</th>
+          <th>Precio con IGV</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Producto 1</td>
+          <td>$531.36</td>
+          <td>18%</td>
+          <td>$95.81</td>
+          <td>$648.00</td>
+        </tr>
+        <!-- Agregar filas adicionales si es necesario -->
+      </tbody>
+    </table>
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
