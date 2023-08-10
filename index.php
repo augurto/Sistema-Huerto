4071e104ec4042f5e1ad0f5ee9403b9db472d10b
Ego <augurto.17@gmail.com>
2023-08-10 16:12:19 -0500

    asd

--- index.php
+++ index.php
@@ -4,30 +4,46 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <title>Precios con y sin IGV</title>
+  <title>Tabla de Precios</title>
 </head>
 <body>
   <div class="container mt-5">
-    <h2>Tabla de Precios con y sin IGV</h2>
+    <h2>Tabla de Precios</h2>
     <table class="table table-bordered">
       <thead>
         <tr>
           <th>Descripción</th>
-          <th>Precio sin IGV</th>
-          <th>Tasa de IGV</th>
-          <th>Monto de IGV</th>
-          <th>Precio con IGV</th>
+          <th>Unidades</th>
+          <th>Precio por Unidad</th>
+          <th>Total</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>Producto 1</td>
-          <td>$531.36</td>
-          <td>18%</td>
-          <td>$95.81</td>
-          <td>$648.00</td>
+          <td>Tostado Mix de 50g</td>
+          <td>80</td>
+          <td>S/.3,60</td>
+          <td>S/.288,00</td>
+        </tr>
+        <tr>
+          <td>Sumac Mix de 50g</td>
+          <td>80</td>
+          <td>S/.3,26</td>
+          <td>S/.260,80</td>
         </tr>
         <!-- Agregar filas adicionales si es necesario -->
+        <tr>
+          <td colspan="3" class="text-right">Subtotal</td>
+          <td>S/.548,80</td>
+        </tr>
+        <tr>
+          <td colspan="3" class="text-right">IGV (18%)</td>
+          <td>S/.98,99</td>
+        </tr>
+        <tr>
+          <td colspan="3" class="text-right">Total</td>
+          <td>S/.647,79</td>
+        </tr>
       </tbody>
     </table>
   </div>
