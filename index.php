6febc5595046843fc4bc260de820e48fb2705bdf
Ego <augurto.17@gmail.com>
2023-08-10 16:14:28 -0500

    con input

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <title>Tabla de Precios</title>
+  <title>Calculadora de Precios</title>
 </head>
 <body>
   <div class="container mt-5">
-    <h2>Tabla de Precios</h2>
+    <h2>Calculadora de Precios</h2>
     <table class="table table-bordered">
       <thead>
         <tr>
@@ -20,29 +20,29 @@
       </thead>
       <tbody>
         <tr>
-          <td>Tostado Mix de 50g</td>
-          <td>80</td>
-          <td>S/.3,60</td>
-          <td>S/.288,00</td>
+          <td><input type="text" class="form-control" id="descripcion1"></td>
+          <td><input type="number" class="form-control" id="unidades1" onchange="calcularTotal(1)"></td>
+          <td><input type="number" class="form-control" id="precio1" onchange="calcularTotal(1)"></td>
+          <td><span id="total1">0</span></td>
         </tr>
         <tr>
-          <td>Sumac Mix de 50g</td>
-          <td>80</td>
-          <td>S/.3,26</td>
-          <td>S/.260,80</td>
+          <td><input type="text" class="form-control" id="descripcion2"></td>
+          <td><input type="number" class="form-control" id="unidades2" onchange="calcularTotal(2)"></td>
+          <td><input type="number" class="form-control" id="precio2" onchange="calcularTotal(2)"></td>
+          <td><span id="total2">0</span></td>
         </tr>
-        <!-- Agregar filas adicionales si es necesario -->
+        <!-- Agregar más filas si es necesario -->
         <tr>
           <td colspan="3" class="text-right">Subtotal</td>
-          <td>S/.548,80</td>
+          <td><span id="subtotal">0</span></td>
         </tr>
         <tr>
           <td colspan="3" class="text-right">IGV (18%)</td>
-          <td>S/.98,99</td>
+          <td><span id="igv">0</span></td>
         </tr>
         <tr>
           <td colspan="3" class="text-right">Total</td>
-          <td>S/.647,79</td>
+          <td><span id="total">0</span></td>
         </tr>
       </tbody>
     </table>
@@ -51,5 +51,29 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script>
+    function calcularTotal(row) {
+      var unidades = parseInt(document.getElementById("unidades" + row).value);
+      var precio = parseFloat(document.getElementById("precio" + row).value);
+      var total = unidades * precio;
+
+      document.getElementById("total" + row).textContent = total.toFixed(2);
+
+      calcularSubtotal();
+    }
+
+    function calcularSubtotal() {
+      var total1 = parseFloat(document.getElementById("total1").textContent);
+      var total2 = parseFloat(document.getElementById("total2").textContent);
+
+      var subtotal = total1 + total2;
+      var igv = subtotal * 0.18;
+      var total = subtotal + igv;
+
+      document.getElementById("subtotal").textContent = subtotal.toFixed(2);
+      document.getElementById("igv").textContent = igv.toFixed(2);
+      document.getElementById("total").textContent = total.toFixed(2);
+    }
+  </script>
 </body>
 </html>
